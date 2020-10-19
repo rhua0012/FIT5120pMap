ee9e50f5ec935997d578b1362b7d127ce1c05ced
rhua0012 <rhua0012@users.noreply.github.com>
2020-10-19 16:20:57 +1100

    Update 20201015_pmap_00e8ba942d1e28f75617_20201019043003_installer-backup.php

--- 20201015_pmap_00e8ba942d1e28f75617_20201019043003_installer-backup.php
+++ 20201015_pmap_00e8ba942d1e28f75617_20201019043003_installer-backup.php
@@ -1,4 +1,5 @@
 <?php
+/* ------------------------------ NOTICE ----------------------------------
 
     If you're seeing this text when browsing to the installer, it means your
     web server is not set up properly.
